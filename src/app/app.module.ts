2dbc53df721656df86c01e9d97ef4c6f83a4d53c
krishnpalchouhan <krishnpalchouhan@gmail.com>
2018-05-29 21:25:38 +0530

    added service worker in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUSComponent } from './contact-us/contact-us.component';
+import { HowItworkComponent } from './how-itwork/how-itwork.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUSComponent,
+    HowItworkComponent
   ],
   imports: [
     BrowserModule
